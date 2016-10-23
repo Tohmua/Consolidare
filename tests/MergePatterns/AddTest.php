b59ae1c95019ea6b17e9d417de22a22ca6a82c6d
Tohmua <oliver.musgrove-wethey@mayden.co.uk>
2016-10-22 17:25:18 -0700

    Add Add Merge Pattern Test

---
+++ tests/MergePatterns/AddTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use Consolidare\MergePatterns\Add;
+use Consolidare\MergePatterns\Exception\CantAddNonNumericsException;
+use Consolidare\MergePatterns\MergePattern;
+use PHPUnit\Framework\TestCase;
+
+class AddTest extends TestCase
+{
+    public function testItImplementsMergePattern()
+    {
+        $this->assertTrue(in_array(
+            MergePattern::class,
+            class_implements(new Add)
+        ));
+    }
+
+    public function testItAdds()
+    {
+        $this->assertEquals(10, (new Add)(4, 6));
+    }
+
+    public function testItAddsNumericStrings()
+    {
+        $this->assertEquals(26, (new Add)('7', "19"));
+    }
+
+    public function testItCantAddStrings()
+    {
+        $this->setExpectedException(CantAddNonNumericsException::class);
+        $this->assertEquals(26, (new Add)('phil', "jess"));
+    }
+
+    public function testItCantAddWithLeftStringValue()
+    {
+        $this->setExpectedException(CantAddNonNumericsException::class);
+        $this->assertEquals(26, (new Add)('phil', 10));
+    }
+
+    public function testItCantAddWithRightStringValue()
+    {
+        $this->setExpectedException(CantAddNonNumericsException::class);
+        $this->assertEquals(26, (new Add)(10, 'jess'));
+    }
+}
